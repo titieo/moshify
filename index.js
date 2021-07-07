e23478ca0c814a7646279917e7311748e61b47fa
Cokaps <locphuc016@gmail.com>
2021-07-07 17:11:52 +0700

    Build blocks,callouts,collapsible,grids,hero,icons,navbar,quote,testimonial components

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import './scss/normalize.css';
 import './scss/index.scss';
+const collapsibles = document.querySelectorAll('.collapsible');
+collapsibles.forEach((item) =>
+    item.addEventListener('click', function () {
+        this.classList.toggle('collapsible--expanded');
+    }),
+);
