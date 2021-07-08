f8921bb5180416feeadb17b04e0ae723efdcfb1d
Cokaps <locphuc016@gmail.com>
2021-07-08 22:31:27 +0700

    Build all needed components and block

--- squoosh.js
+++ squoosh.js
@@ -5,10 +5,10 @@ const { ImagePool } = require('@squoosh/lib');
 const imagePool = new ImagePool();
 
 // Define your paths:
-const imagesToOptimizePath = './src/rawImages/';
-// const moveToPath =
-//     './_originalRawImages/'; /* Uncomment this if you want to keep files */
-const optimizeToPath = './src/img/';
+const imagesToOptimizePath = './rawImages/';
+const moveToPath =
+    './originalRawImages/'; /* Uncomment this if you want to keep files */
+const optimizeToPath = './images/';
 
 (async () => {
     const files = await fs.promises.readdir(imagesToOptimizePath);
@@ -22,7 +22,10 @@ const optimizeToPath = './src/img/';
 
 async function libSquooshOptimize(file) {
     const imagePath = path.join(imagesToOptimizePath, file);
-    // const moveOriginalToPath = path.join(moveToPath, file); /* Uncomment this if you want to keep files */
+    const moveOriginalToPath = path.join(
+        moveToPath,
+        file,
+    ); /* Uncomment this if you want to keep files */
     const saveOptimizedImageToPath = path.join(
         optimizeToPath,
         path.parse(file).name,
@@ -57,5 +60,8 @@ async function libSquooshOptimize(file) {
     }
 
     // move original image async
-    // await fs.promises.rename(imagePath, moveOriginalToPath); /* Uncomment this if you want to keep files */
+    await fs.promises.rename(
+        imagePath,
+        moveOriginalToPath,
+    ); /* Uncomment this if you want to keep files */
 }
