ed755282a2e307e1d9ec1de487489175604a79e3
loctran016 <locphuc016@gmail.com>
2021-07-09 04:08:55 +0000

    Deploying to gh-pages from @ loctran016/moshify@4ad208e48155b1ddcfa2aed6a536d190e26dd8c0 🚀

---
+++ assets/index.86cc0592.js
@@ -0,0 +1,1 @@
+import{a as e}from"./vendor.1d91cf28.js";e.init();document.querySelectorAll(".collapsible").forEach((e=>e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
