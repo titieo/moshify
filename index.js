4ad208e48155b1ddcfa2aed6a536d190e26dd8c0
Cokaps <locphuc016@gmail.com>
2021-07-09 11:07:42 +0700

    Add animation and fix misc. Repair to push to github

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import './scss/normalize.css';
 import './scss/index.scss';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+// You can also use <link> for styles
+// ..
+AOS.init();
+
 const collapsibles = document.querySelectorAll('.collapsible');
 collapsibles.forEach((item) =>
     item.addEventListener('click', function () {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+
+import { minifyHtml } from 'vite-plugin-html';
 
 export default defineConfig({
-  server: {
-    open: true,
-  },
+    server: {
+        open: true,
+    },
+    publicDir: 'images',
+    build: { terserOptions: { ecma: '5' } },
+    plugins: [minifyHtml()],
 });
